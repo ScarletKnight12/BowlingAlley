2efeccfbf1872a99239148833e65e54de8763784
Annapoorani1920 <annapoorani.a@students.iiit.ac.in>
2022-04-20 18:34:29 +0530

    UI top scores enable and disable logic

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.IO;
+using System.Collections;
 
 public class UIScript : MonoBehaviour
 {
@@ -12,18 +13,19 @@ public class UIScript : MonoBehaviour
     public static string inputText = "Guest";
     public TMP_InputField inputField;
     public TextMeshProUGUI highScoresText;
+    public Canvas CanvasA, CanvasB;
     
     string filePath;
 
 #region unity methods
     // Start is called before the first frame update
     void Start()
-    {   
-        //set file path where the score data will be persisted.
-        filePath = Application.persistentDataPath + "/bowling_data.txt";
+    {
+
+         filePath = Application.persistentDataPath + "/bowling_data.txt";
     }
 
-#endregion
+    #endregion
 
     public void loadBowlingScene() {
         inputText = inputField.text;
@@ -38,8 +40,9 @@ public class UIScript : MonoBehaviour
         Application.Quit();
     }
 
-    //Function which gets top 10 scores from score records and sets them in UI. Very basic and inefficient at this point 
+    //Function which gets top 10 scores from score records and sets them in UI.  
     public void getTopScores() {
+
         if (File.Exists(filePath)) {
             //fetch score records from filepath.
             List<ScoreRecord> scoreRecords = new List<ScoreRecord>();
@@ -64,8 +67,27 @@ public class UIScript : MonoBehaviour
                     resultText += "" + (i+1) + ". " + resultRecords[i].getPlayerName() + "    " + resultRecords[i].getScore() + "\n";
                 }
             }
+
+            
+            CanvasA.gameObject.SetActive(false);
+            CanvasB.gameObject.SetActive(true);
+
+
             highScoresText.SetText(resultText);
         }
+        else
+        {
+            CanvasA.gameObject.SetActive(false);
+            CanvasB.gameObject.SetActive(true);
+            highScoresText.SetText("No Scores Available...");
+        }
+    }
+
+    public void getBackHome()
+    {
+        CanvasA.gameObject.SetActive(true);
+        CanvasB.gameObject.SetActive(false);
+
     }
 
 }
